4cc990c401707cb6f15b8dc0c13ace0e2a92ae77
Elizabeth Figura <zfigura@codeweavers.com>
2021-03-09 01:16:53 +0200

    server: Create in-process synchronization objects for processes.
    Signed-off-by: Vasiliy Stelmachenok <ventureo@cachyos.org>

--- server/process.h
+++ server/process.h
@@ -86,6 +86,7 @@ struct process
     struct list          rawinput_entry;  /* entry in the rawinput process list */
     struct list          kernel_object;   /* list of kernel object pointers */
     struct pe_image_info image_info;      /* main exe image info */
+    struct inproc_sync  *inproc_sync;     /* in-process synchronization object */
     int                  esync_fd;        /* esync file descriptor (signaled on exit) */
     unsigned int         fsync_idx;
     struct cpu_topology_override cpu_override; /* Overridden CPUs to host CPUs mapping. */
