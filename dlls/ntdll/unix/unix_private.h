b93d235dfa69e0ecf250cf746e1c32a3692d01be
Elizabeth Figura <zfigura@codeweavers.com>
2021-03-12 23:04:17 +0200

    ntdll: Cache in-process synchronization objects.

--- dlls/ntdll/unix/unix_private.h
+++ dlls/ntdll/unix/unix_private.h
@@ -221,6 +221,8 @@ extern NTSTATUS load_start_exe( WCHAR **image, void **module );
 extern ULONG_PTR redirect_arm64ec_rva( void *module, ULONG_PTR rva, const IMAGE_ARM64EC_METADATA *metadata );
 extern void start_server( BOOL debug );
 
+extern pthread_mutex_t fd_cache_mutex;
+
 extern unsigned int server_call_unlocked( void *req_ptr );
 extern void server_enter_uninterrupted_section( pthread_mutex_t *mutex, sigset_t *sigset );
 extern void server_leave_uninterrupted_section( pthread_mutex_t *mutex, sigset_t *sigset );
@@ -401,6 +403,8 @@ extern NTSTATUS wow64_wine_spawnvp( void *args );
 #endif
 
 extern void dbg_init(void);
+
+extern void close_inproc_sync_obj( HANDLE handle );
 
 extern NTSTATUS call_user_apc_dispatcher( CONTEXT *context_ptr, ULONG_PTR arg1, ULONG_PTR arg2, ULONG_PTR arg3,
                                           PNTAPCFUNC func, NTSTATUS status );
